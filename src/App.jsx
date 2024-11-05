0f6c7a3ef29de59e6b225cfd4b53a262115a1646
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-11-05 11:52:43 -0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
   return (
-    <>
     <div>
      <header>
       <nav>
@@ -21,7 +17,6 @@ const App = () => {
       </div>
      </body>
      </div>
-     </>
   );
  }
 
